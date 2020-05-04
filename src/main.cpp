2deee0ed0c842e17e3f6bf952d2f7f2b9b5a2310
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-05-04 22:52:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,33 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include <LiquidCrystal.h>
-#include <words.h>
 #include <main.h>
 
-LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_E, LCD_PIN_DB4, LCD_PIN_DB5, LCD_PIN_DB6, LCD_PIN_DB7);
+#define DEBUG true
+
+const uint8_t mosiPin = 8;
+const uint8_t misoPin = 7; //CLK INH, Latch on the output is connected to the SS output.
+const uint8_t buzzerPin = 6;
+
+Keypad keypad(mosiPin, misoPin, buzzerPin, true);
 
 void setup()
 {
-    pinMode(BRT_INPUT, INPUT);
-    pinMode(LCD_PIN_BRT, OUTPUT);
-    lcd.begin(16, 2);
-    lcd.setCursor(0, 0);
-    lcd.print(lcd_txt_searching_players);
+  pinMode(mosiPin, OUTPUT);
+  pinMode(misoPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  
+  #if defined(DEBUG)
+  Serial.begin(9600);
+  #endif
 }
 
 void loop()
 {
-  updateLcdBrightness();
-}
-
-// This function will update the LCD brightness
-void updateLcdBrightness()
-{
-  int externalBright = max(300, analogRead(BRT_INPUT)); //R1(photo) + R2(100kOm)
-  int lcdBright = map(externalBright, 100, 1024, 256, 0);
-  analogWrite(LCD_PIN_BRT, lcdBright);
+  if (!keypad.isKeypadUpdated()) {
+    return;
+  }
+  uint8_t keypadSymbol = keypad.getKeypadSymbol();
+  if (keypadSymbol != Keypad::keyMultiSymbol && keypadSymbol != Keypad::keyReleasedSymbol) {
+    #if defined(DEBUG)
+    Serial.print(int(keypadSymbol));
+    #endif 
+  }
 }
--- src/main.h
+++ src/main.h
@@ -1,11 +1,2 @@
-#define LCD_PIN_RS 12 //Pin 4 On the LCD
-#define LCD_PIN_E 11  //Pin 6 On the LCD
-#define LCD_PIN_DB4 5 //Pin 11 On the LCD
-#define LCD_PIN_DB5 4 //Pin 12 On the LCD
-#define LCD_PIN_DB6 3 //Pin 13 On the LCD
-#define LCD_PIN_DB7 2 //Pin 14 On the LCD
-
-#define LCD_PIN_BRT 10 //Arduino pin to write the LCD brightness
-#define BRT_INPUT A0   //Arduino pin to read external brightness
-
-void updateLcdBrightness();
+#include <Arduino.h>
+#include <Keypad.h>
