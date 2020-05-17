443ff1348d45e0ee667cba32e3e04e44e93ae64c
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-05-18 01:01:33 +0300

    Add StringAssets

---
+++ include/StringAssets.h
@@ -0,0 +1,14 @@
+namespace StringAssets {
+    const char mainMenu[] = "main menu";
+    const char intro[] = "Intro";
+    const char pressAnyKey[] = "Press any key";
+    const char games[] = "Games";
+    const char roborally[] = "Roborally";
+    const char configs[] = "Configs";
+    const char contrast[] = "Contrast";
+    const char about[] = "About";
+    const char backlightOn[] = "Backlight:on";
+    const char backlightOff[] = "Backlight:off";
+    const char soundsOn[] = "Sounds:on";
+    const char soundsOff[] = "Sounds:off";
+}
