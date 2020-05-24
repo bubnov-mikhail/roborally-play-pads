1502a981deec0d45469fb78a2e41ec7a4c94f499
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-05-24 22:13:34 +0300

    Fix LcdAssets

--- lib/LcdAssets/LcdAssets.h
+++ lib/LcdAssets/LcdAssets.h
@@ -10,9 +10,9 @@ namespace LcdAssets {
     const unsigned char batteryFull[] PROGMEM = {0x7e, 0x7d, 0x7d, 0x7d, 0x7e};
     const unsigned char batteryHalf[] PROGMEM = {0x7e, 0x71, 0x71, 0x71, 0x7e};
     const unsigned char batteryLow[] PROGMEM = {0x7e, 0x41, 0x41, 0x41, 0x7e};
-    const unsigned char progressBarSideBorder PROGMEM = 0xff;
-    const unsigned char progressBarEmptyBody PROGMEM = 0x81;
-    const unsigned char progressBarFilledBody PROGMEM = 0xbd;
+    const unsigned char progressBarSideBorder[] PROGMEM = {0xff};
+    const unsigned char progressBarEmptyBody[] PROGMEM = {0x81};
+    const unsigned char progressBarFilledBody[] PROGMEM = {0xbd};
     const unsigned char menuHeader[] PROGMEM = {0xf3, 0x79, 0x3c, 0x9e, 0xcf, 0xe7};
     const unsigned char menuItem[] PROGMEM = {0xff, 0x00, 0x00, 0x00};
     const unsigned char menuItemSelected[] PROGMEM = {0xff, 0xff, 0xff, 0x00};
