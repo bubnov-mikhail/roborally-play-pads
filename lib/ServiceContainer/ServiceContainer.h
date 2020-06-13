7acf264fee7341980f9ad097601a2a4d80a9cf46
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-06-14 01:09:10 +0300

    Add BitmapLoader to the ServiceContainer

--- lib/ServiceContainer/ServiceContainer.h
+++ lib/ServiceContainer/ServiceContainer.h
@@ -5,20 +5,23 @@
 #include <Nokia_LCD.h>
 #include <MenuRenderer.h>
 #include <Headline.h>
+#include <BitmapLoader.h>
 
 class ServiceContainer {
     public:
-        ServiceContainer(ConfigStorage* _configStorage, Nokia_LCD* _lcd, Keypad* _keypad, Headline* _headline, uint8_t _buzzerPin);
+        ServiceContainer(ConfigStorage* _configStorage, Nokia_LCD* _lcd, Keypad* _keypad, Headline* _headline, BitmapLoader* _bitmapLoader, uint8_t _buzzerPin);
         ConfigStorage* getConfigStorage(void);
         Nokia_LCD* getLcd(void);
         Keypad* getKeypad(void);
         Headline* getHeadline(void);
         MenuRenderer* getMenuRenderer(void);
+        BitmapLoader* getBitmapLoader(void);
     private:
         ConfigStorage* configStorage;
         MenuRenderer* menuRenderer;
         Nokia_LCD* lcd;
         Keypad* keypad;
         Headline* headline;
+        BitmapLoader* bitmapLoader;
         uint8_t buzzerPin;
 };
