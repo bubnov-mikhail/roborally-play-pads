0bdda21e718c7f95e74a40b0478e7135dfe246ef
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-06-14 15:12:06 +0300

    Add new string assets

--- include/StringAssets.h
+++ include/StringAssets.h
@@ -1,11 +1,12 @@
 namespace StringAssets {
-    const char mainMenu[] = "main menu";
+    const char mainMenu[] = "Main menu";
     const char intro[] = "Intro";
     const char pressAnyKey[] = "Press any key";
     const char games[] = "Games";
     const char roborally[] = "Roborally";
     const char configs[] = "Configs";
     const char contrast[] = "Contrast";
+    const char loading[] = "Loading";
     const char about[] = "About";
     const char backlightOn[] = "Backlight:on";
     const char backlightOff[] = "Backlight:off";
@@ -18,4 +19,7 @@ namespace StringAssets {
 
     //Roborally game
     const char enterCard[] = "Enter card";
+    const char confirmCard[] = "Confirm card";
+    const char waitingForOthers[] = "Waiting";
+    const char yourMove[] = "Your move";
 }
