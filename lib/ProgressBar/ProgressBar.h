c569103948ff2f24e6cd0e53bc819514886bec7a
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-08-31 23:07:24 +0300

    Add new class ProgressBar

---
+++ lib/ProgressBar/ProgressBar.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Nokia_LCD.h>
+#include <LcdAssets.h>
+
+class ProgressBar {
+    public:
+        ProgressBar(Nokia_LCD* _lcd, uint8_t _min, uint8_t _max, uint8_t _lcdY, bool _riseOnly);
+        void render(uint8_t percent);
+    private:
+        Nokia_LCD* lcd;
+        uint8_t min;
+        uint8_t max;
+        uint8_t lastValue;
+        uint8_t lcdY;
+        bool riseOnly;
+        bool initRequired = true;
+};
