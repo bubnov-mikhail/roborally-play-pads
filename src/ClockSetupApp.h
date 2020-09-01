937f83c8a8df32a67da837b362f9398715d794ed
Mihail Bubnov <mihail.bubnov@mediaopt.de>
2020-09-01 23:06:43 +0300

    Add ClockSetupApp

---
+++ src/ClockSetupApp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "AbstractApp.h"
+#include <ProgressBar.h>
+
+class ClockSetupApp : public AbstractApp {
+    public:
+        void execute(void);
+    private:
+        enum ClockComponent {HOUR, MINUTE, DAY, MONTH, YEAR};
+        ClockComponent currentComponent;
+        short int componentValue;
+        unsigned long blinkLastUpdated;
+        bool selectComponent = true; //Highligh a selected component with inverted color
+        const static unsigned short int blinkRefreshMilis = 500;
+        const static unsigned short int yearOffset = 1952;
+        void drawComponents(void);
+        void printValue(short int value);
+        void setComponentValue(void);
+        void nextComponent(void);
+        void prevComponent(void);
+        void increaseValue(void);
+        void decreaseValue(void);
+        void changeComponent(bool increase);
+        void changeValue(bool increase);
+};
